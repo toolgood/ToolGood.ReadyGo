7b9d7c8e9bb8ac3cd1ca906df0b01ac351efdde8
toolgood <at6180686@qq.com>
2016-10-28 19:44:18 +0800

    重大修改-20161028

---
+++ ToolGood.ReadyGo/Caches/IUseCache.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ToolGood.ReadyGo.Caches
+{
+    public interface IUseCache
+    {
+        /// <summary>
+        /// 使用缓存
+        /// </summary>
+        /// <param name="second"></param>
+        /// <param name="cacheTag"></param>
+        /// <param name="cacheService"></param>
+        void useCache(int second, string cacheTag = null, ICacheService cacheService = null);
+    }
+}
