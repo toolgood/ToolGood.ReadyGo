f09fd77a66f88712d5cc6001aaae577b0f1c4a91
toolgood <toolgood@qq.com>
2018-01-26 23:20:48 +0800

    3.0.29版本，代码整理

---
+++ ToolGood.ReadyGo3/DataCentxt/QTableModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ToolGood.ReadyGo3.DataCentxt
+{
+    /// <summary>
+    /// 适用于MVC 中间值的转值类
+    /// 前端传值 =》 转成MVC Tb类（基类：QTableModel） =》 通过GetChange方法 传值给 QTable =》 转成SQL语句 =》 保存到数据库
+    /// </summary>
+    public abstract class QTableModel
+    {
+        private Dictionary<string, object> _dictionary = new Dictionary<string, object>();
+        protected void SetValue(string txt, object value) { _dictionary[txt] = value; }
+        protected T GetValue<T>(string txt) { return _dictionary.ContainsKey(txt) ? (T)_dictionary[txt] : default(T); }
+
+
+        protected void SetString(string txt, string html)
+        {
+            if (string.IsNullOrEmpty(html)) return;
+            html = Regex.Replace(html, @"(<!--.*?-->)", "", RegexOptions.IgnoreCase);
+            html = Regex.Replace(html, @"<style[^>]*?>[\s\S]*?</style>", "", RegexOptions.IgnoreCase);
+            html = Regex.Replace(html, @"<script([^>])*>(\w|\W)*?</script([^>])*>", "", RegexOptions.IgnoreCase);
+            html = Regex.Replace(html, "<[^>]+>", "");
+            html = Regex.Replace(html, @"\s+", " ");
+            html = html.Replace("<", "&lt;").Replace(">", "&gt;");
+            _dictionary[txt] = html;
+        }
+        public Dictionary<string, object> GetChange() { return _dictionary; }
+    }
+}
