50333cc15df0d9a6f8b68913208de12627d170ae
toolgood@qq.com <toolgood@qq.com>
2018-07-17 14:08:38 +0800

    3.1.0.32版本

--- ToolGood.ReadyGo3/PetaPoco/Providers/SqlServerDatabaseProvider.cs
+++ ToolGood.ReadyGo3/PetaPoco/Providers/SqlServerDatabaseProvider.cs
@@ -20,9 +20,14 @@ namespace ToolGood.ReadyGo3.PetaPoco.Providers
 #else
             return GetFactory(
                 "System.Data.SqlClient.SqlClientFactory, System.Data, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089",
+
                 "System.Data.SqlClient.SqlClientFactory, System.Data.SqlClient, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a",
                 "System.Data.SqlClient.SqlClientFactory, System.Data.SqlClient, PublicKeyToken=b03f5f7f11d50a3a",
-                "System.Data.SqlClient.SqlClientFactory, System.Data.SqlClient"
+                "System.Data.SqlClient.SqlClientFactory, System.Data.SqlClient",
+
+                "System.Data.SqlClient.SqlClientFactory, System.Data, Culture=neutral, PublicKeyToken=b77a5c561934e089",
+                "System.Data.SqlClient.SqlClientFactory, System.Data, PublicKeyToken=b77a5c561934e089",
+                "System.Data.SqlClient.SqlClientFactory, System.Data"
                 );
 #endif
         }
