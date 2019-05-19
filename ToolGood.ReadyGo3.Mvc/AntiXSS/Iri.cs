6a5c521977294306f39b5e4e21d979f6e96be4d6
toolgood <toolgood@qq.com>
2019-05-19 12:25:12 +0800

    修改 删除QTable类型

--- ToolGood.ReadyGo3.Mvc/AntiXSS/Iri.cs
+++ ToolGood.ReadyGo3.Mvc/AntiXSS/Iri.cs
@@ -1,6 +1,14 @@
-﻿using System;using System.Collections.Generic;using System.Linq;using System.Text;namespace ToolGood.ReadyGo3.Mvc.AntiXSS{    public class Iri
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ToolGood.ReadyGo3.Mvc.AntiXSS
+{
+    public class Iri
     {
         public string Value { get; set; }
         public bool IsAbsolute => !string.IsNullOrEmpty(Scheme);
         public string Scheme { get; set; }
-    }}
+    }
+}
