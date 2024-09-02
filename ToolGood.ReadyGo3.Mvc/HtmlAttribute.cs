fd8a91fb97fb66d843d25706522b1b262cf8a10a
linzhijun <at6180686@qq.com>
2024-09-02 10:38:53 +0800

    fix

--- ToolGood.ReadyGo3.Mvc/HtmlAttribute.cs
+++
@@ -1,1 +0,0 @@
-﻿using System;using System.Collections.Generic;using System.Linq;using System.Text;namespace ToolGood.ReadyGo3.Attributes{    /// <summary>    /// 过滤Html XSS攻击    /// </summary>    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = false)]    public class HtmlAttribute : Attribute    {    }}
