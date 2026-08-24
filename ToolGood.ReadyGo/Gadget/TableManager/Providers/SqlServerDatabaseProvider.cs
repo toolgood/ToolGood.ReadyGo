819a1c2041ebd04e45133a9651b5aa95dd6b31ca
linzhijun <at6180686@qq.com>
2026-08-24 14:41:32 +0800

    fix

--- ToolGood.ReadyGo/Gadget/TableManager/Providers/SqlServerDatabaseProvider.cs
+++ ToolGood.ReadyGo/Gadget/TableManager/Providers/SqlServerDatabaseProvider.cs
@@ -137,7 +137,7 @@ namespace ToolGood.ReadyGo.Gadget.TableManager.Providers
             var isRequired = ci.Required;
             if (ci.IsSerializedAsInt) return CreateField(ti, ci, "int", ci.FieldLength, isRequired);
             if (ci.IsSerializedAsLong) return CreateField(ti, ci, "bigint", ci.FieldLength, isRequired);
-            if (ci.IsSerializedAsString) return CreateField(ti, ci, "Text", "", false);
+            if (ci.IsSerializedAsString) return CreateField(ti, ci, ci.IsText ? "Text" : "nvarchar", ci.IsText ? "" : (string.IsNullOrEmpty(ci.FieldLength) ? "4000" : ci.FieldLength), false);
             if (type.IsEnum) return CreateField(ti, ci, "int", ci.FieldLength, isRequired);
             if (type == typeof(string)) return CreateField(ti, ci, ci.IsText ? "Text" : "nvarchar", ci.IsText ? "" : (string.IsNullOrEmpty(ci.FieldLength) ? "4000" : ci.FieldLength), isRequired);
             if (type == typeof(Byte[])) return CreateField(ti, ci, "BLOB", ci.FieldLength, false);
